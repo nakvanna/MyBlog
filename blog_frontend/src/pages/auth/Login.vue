39641b54f79bc92a4ee05280e28fffb098732171
nakvanna <nakvannamcu@gmail.com>
2021-10-16 22:48:50 +0700

    update auth ui

--- blog_frontend/src/pages/auth/Login.vue
+++ blog_frontend/src/pages/auth/Login.vue
@@ -1,36 +1,85 @@
 <template>
-  <q-page class="q-ma-md">
-    <q-toolbar class="bg-brown-5">
-      <q-toolbar-title class="text-center text-bold ">
-        ការចូលប្រើប្រាស់
-        <div class="float-right">
-          <q-btn to="/register" dense rounded outline>Register</q-btn>
-        </div>
-      </q-toolbar-title>
-    </q-toolbar>
-    <q-card class="q-pt-md">
-      <q-card-section>
-        <q-form
-          @submit=""
-          @reset="lgUser.md.user.clear()"
+  <q-page
+    class="q-ma-md"
+  >
+    <div class="full-width" style="padding-top: 50px">
+      <div class="row justify-center text-center">
+        <!--        left section-->
+        <div
+          class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 my-div-left"
+          style="padding-top: 100px"
         >
-          <q-input
-            v-model="lgUser.state.user.data.email_or_username"
-            label="Email or Username"
-            lazy-rules
-            :rules="[ val => val && val.length > 0 || 'Please type something']"
-          />
-          <q-input
-            v-model="lgUser.state.user.data.password"
-            label="Password"
-            type="password"
-            lazy-rules
-            :rules="[ val => val && val.length > 0 || 'Please type something']"
-          />
-          <q-btn type="submit" rounded color="teal-5" class="full-width">Login</q-btn>
-        </q-form>
-      </q-card-section>
-    </q-card>
+          <div class="font-nosifer text-red-5" style="font-size: 36px">
+            Blogger
+          </div>
+          <div class="font-play-ball" style="font-size: 28px">
+            Connect with friends and the world around you on <span class="text-red-5">Blogger</span>.
+          </div>
+
+        </div>
+
+        <!--        Right section-->
+        <div
+          class="col-xs-12 col-sm-12 col-md col-lg col-xl">
+          <div class="row my-div-right" style="padding-top: 80px;">
+            <q-card style="width: 350px">
+              <q-card-section>
+                <q-form
+                  @submit=""
+                  @reset="lgUser.md.user.clear()"
+                >
+                  <q-input
+                    outlined
+                    v-model="lgUser.state.user.data.email_or_username"
+                    label="Email or Username"
+                    lazy-rules
+                    :rules="[ val => val && val.length > 0 || 'Please type something']"
+                  />
+                  <q-input
+                    outlined
+                    v-model="lgUser.state.user.data.password"
+                    label="Password"
+                    type="password"
+                    lazy-rules
+                    :rules="[ val => val && val.length > 0 || 'Please type something']"
+                  />
+                  <q-btn
+                    type="submit"
+                    rounded color="teal-5"
+                    class="full-width font-play-ball"
+                  >Login
+                  </q-btn>
+                </q-form>
+              </q-card-section>
+              <q-separator/>
+              <q-card-actions>
+                <q-space/>
+                <q-btn
+                  @click="state.dialog.create = true"
+                  color="blue" class="font-play-ball"
+                  flat label="Register"
+                >
+                  <RegisterDialog v-model="state.dialog.create"/>
+                </q-btn>
+              </q-card-actions>
+            </q-card>
+          </div>
+        </div>
+      </div>
+
+      <!--      footer    -->
+      <div class="row text-center">
+        <q-footer class="bg-white text-black q-pa-md" elevated>
+          <div class="full-width font-play-ball text-h6">
+            POLICIES
+          </div>
+          <div class="full-width font-play-ball">
+            Fantastic value! I'm not sure how I'd do without it. It’s worth its weight in gold and paid back for itself
+            many times.
+          </div>
+        </q-footer>
+      </div>
+    </div>
   </q-page>
 </template>
 
@@ -38,12 +87,18 @@
 
 import {reactive} from "@vue/composition-api";
 import {loginUser} from "pages/auth/store/auth.store";
+import RegisterDialog from "pages/auth/RegisterDialog.vue";
 
 export default {
   name: "Login",
+  components: {RegisterDialog},
   setup: () => {
     const lgUser = loginUser()
-    const state = reactive({})
+    const state = reactive({
+      dialog: {
+        create: false
+      }
+    })
     const md = {}
     return {
       state,
@@ -54,6 +109,27 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="sass" scoped>
+.my-div-left
+  body.screen--xs &
+    padding-top: 10px !important
+
+  body.screen--sm &
+    padding-top: 20px !important
+
+.my-div-right
+  body.screen--xs &
+    justify-items: center
+    justify-content: center
+    align-content: center
+    align-items: center
+    padding-top: 30px !important
+
+  body.screen--sm &
+    justify-items: center
+    justify-content: center
+    align-content: center
+    align-items: center
+    padding-top: 30px !important
 
 </style>
---
+++ blog_frontend/src/pages/auth/RegisterDialog.vue
@@ -0,0 +1,91 @@
+<template>
+  <q-dialog v-model="state.dialog.show">
+    <q-card style="width: 450px">
+      <q-card-section>
+        <div class="text-h6 font-nosifer">Register</div>
+      </q-card-section>
+      <q-separator/>
+      <q-card-section>
+        <q-form
+          @submit=""
+          @reset=""
+        >
+          <q-input
+            outlined
+            v-model="crUser.state.user.data.name"
+            label="Name"
+            lazy-rules
+            :rules="[ val => val && val.length > 0 || 'Please type something']"
+          />
+          <q-input
+            outlined
+            v-model="crUser.state.user.data.username"
+            label="Username"
+            lazy-rules
+            :rules="[ val => val && val.length > 0 || 'Please type something']"
+          />
+          <q-input
+            outlined
+            v-model="crUser.state.user.data.email"
+            label="E-mail"
+            lazy-rules
+            :rules="[ val => val && val.length > 0 || 'Please type something']"
+          />
+          <q-input
+            outlined
+            v-model="crUser.state.user.data.password"
+            label="Password"
+            type="password"
+            lazy-rules
+            :rules="[ val => val && val.length > 0 || 'Please type something']"
+          />
+          <q-input
+            outlined
+            v-model="crUser.state.user.data.password_confirmation"
+            label="Confirm password"
+            type="password"
+            lazy-rules
+            :rules="[ val => val && val.length > 0 || 'Please type something']"
+          />
+          <q-btn type="submit" rounded color="teal-5" class="full-width">Register</q-btn>
+        </q-form>
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script lang="ts">
+import {computed, defineComponent, reactive} from "@vue/composition-api";
+import {registerUser} from "pages/auth/store/auth.store";
+
+export default defineComponent({
+  name: "RegisterDialog",
+  props: {
+    value: {
+      type: Boolean,
+      default: false,
+    }
+  },
+  setup: (props: any, ctx) => {
+    const crUser = registerUser()
+    const state = reactive({
+      dialog: {
+        show: computed({
+          get: () => props.value,
+          set: (val) => ctx.emit('input', val)
+        })
+      }
+    })
+    const md = {}
+    return {
+      state,
+      md,
+      crUser
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
